64d6996add12d2932c8c9439a8f3f502e0066f93
sam-uit <25410291@ms.uit.edu.vn>
2026-07-20 21:44:23 +0800

    Hàm lấy giá trị ngẫu nhiên từ các lựa chọn khả thi

--- exercises/l01/kbb.py
+++ exercises/l01/kbb.py
@@ -8,10 +8,18 @@
 # - Kéo > Bao
 # - Búa > Kéo
 # - Bao > Búa
+
+import random
+
 # dict chứa các lựa chọn khả thi
 kbb_dict = {
     "keo": "Kéo",
     "bua": "Búa",
     "bao": "Bao"
 }
+
+# get_kbb_choice: trả về một lựa chọn ngẫu nhiên từ kbb_dict
+def get_kbb_choice():
+    """Trả về một lựa chọn ngẫu nhiên từ kbb_dict"""
+    return random.choice(tuple(kbb_dict.keys()))
 
