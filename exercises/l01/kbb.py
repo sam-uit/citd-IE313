59eece87c8ff584705daaf9c8e41ad7e18a9c309
sam-uit <25410291@ms.uit.edu.vn>
2026-07-20 20:09:38 +0800

    kbb IE313 - Buổi 01 - Hoạt Động 02

---
+++ exercises/l01/kbb.py
@@ -0,0 +1,10 @@
+# IE313 - Buổi 01 - Hoạt Động 02
+# MSSV:
+# Họ Tên:
+# Github:
+# Bài Tập
+# Viết chương trình mô phỏng trò chơi Kéo - Búa - Bao giữa người và máy.
+# Quy ước:
+# - Kéo > Bao
+# - Búa > Kéo
+# - Bao > Búa
